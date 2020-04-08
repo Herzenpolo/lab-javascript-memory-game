57ddb8cf45841dd4e5d112f45e2a0c26ef724768
Herzen Cortes <herzencc@icloud.com>
2020-04-08 00:20:08 -0400

    iteration 3

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,35 @@
+const shuffle = (arr) => {
+  arr.sort(() => Math.random()-0.5)
+}
+
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards() {
+    this.deck = [...this.cards]
+    return shuffle(this.cards)
+  }
+  
+  
+  checkIfPair(card1, card2) {
+      this.pairsClicked += 1
+      if(card1 === card2){
+        this.pairsGuessed += 1
+        return true;
+      } else {
+        return false;
+      }
+  }
+  
+  isFinished() {
+    if(this.pairsClicked = 0) {
+      return false
+    }
+  }
 }
